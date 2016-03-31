2fbf808ee97b5da726af578574add1b2e7e4a66b
Louisa Reese <louisa.e.reese@gmail.com>
2016-03-31 15:49:22 -0600

    able to add to receipt on click, need to not duplicate, then complete the total and tax fields:

--- app.js
+++ app.js
@@ -1,12 +1,29 @@
-var app = angular.module('cashApp', {});
+var app = angular.module('cashApp', []);
 
 app.controller('cashRegister', function ($scope) {
-  $scope = {};
-  $scope.food = {};
-  $scope.food.pizza = {};
-  $scope.food.salad = {};
-  $scope.food.burger = {};
-  $scope.food.cheeseburger = {};
+  $scope.food = [{
+    name: 'Pizza',
+    price: 9.99,
+    count: 0
+  },{
+    name: 'Salad',
+    price: 5.99,
+    count: 0
+  },{
+    name: 'Burger',
+    price: 12.99,
+    count: 0
+  },{
+    name: 'Cheeseburger',
+    price: 12.99,
+    count: 0
+  }];
+
   $scope.receipt = {};
-  $scope.total = {};
+  $scope.receipt.receiptArray = [];
+  $scope.receipt.addItem = function(food, array) {
+    array.push({name: food.name, count: food.count+=1, price: (food.price * food.count)});
+    return array;
+  };
+  $scope.cost = {total: 0, tax: 0};
 });
